78174c11a6dfee5abae786ac46e9d4a9d331caf2
Alexandru-Nicolae DUNĂRENŢU (102451) <alexandru.dunarentu@stud.etti.upb.ro>
2020-07-25 23:25:43 +0200

    fix mobile nav

--- shared.js
+++ shared.js
@@ -3,13 +3,14 @@ var toggleMenu = document.querySelector(".hamburger-menu");
 var showMenu = document.querySelector(".mobile__nav");
 var checkout = document.querySelector(".checkout");
 var toggleCheckout = document.querySelectorAll(".cart-button");
-
-for (var i = 0; i < toggleCheckout.length; i++) {
+if (toggleCheckout) {
+  for (var i = 0; i < toggleCheckout.length; i++) {
     toggleCheckout[i].addEventListener("click", function () {
       checkout.style.transform = "translateY(0)";
       backdrop.style.display = "block";
     });
   }
+}
 
 toggleMenu.addEventListener("click", function () {
   backdrop.style.display = "block";
@@ -18,8 +19,8 @@ toggleMenu.addEventListener("click", function () {
 
 backdrop.addEventListener("click", function () {
   backdrop.style.display = "none";
-  checkout.style.transform = "translateY(-300%)";
+  if (checkout) {
+    checkout.style.transform = "translateY(-300%)";
+  }
   showMenu.style.transform = "translateX(-100%)";
 });
-
-
